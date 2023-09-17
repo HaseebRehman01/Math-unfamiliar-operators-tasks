a9d4d649fde0eb48e3fc9091ae7a3133c051a128
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-09-17 23:53:41 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// MATH EXPRESSIONS
+// TAsk 1
+
+// var name = 10
+// document.write("Result"+"<br>")
+// document.write("The value of a is ;  "+  name +"<br>")
+// document.write("........................"+"<br>"+"<br>")
+// ++name
+// document.write("The value of ++a is ;  "+  name +"<br>")
+// document.write("Now The value of a is ;  "+  name +"<br>"+"<br>")
+
+// document.write("The value of a++ is ;  "+  name +"<br>")
+// name++
+// document.write(" Now The value of a is ;  "+  name +"<br>"+"<br>")
+
+
+
+// --name
+// document.write(" The value of --a is ;  "+  name +"<br>")
+// document.write(" Now The value of a is ;  "+  name +"<br>"+"<br>")
+
+
+// document.write("  The value of a-- is ;  "+  name +"<br>")
+// name--
+// document.write(" Now The value of a is ;  "+  name +"<br>")
+
+
+// Task 2
+var a = 2
+document.write ("a : " +a+"<br>")
+var b = 1
+document.write ("b : " +b+"<br>")
+var result = --a - --b + ++b  + b--
+document.write("Result of --a - --b + ++b  + b-- : "+ result+"<br>")
+document.write("--a = 1"+"<br>" +"--b = 0"+"<br>" +"++b = 1"+"<br>" +"b-- = 1" +"<br>" +"1 - 0 + 1 + 1 = 3")
+

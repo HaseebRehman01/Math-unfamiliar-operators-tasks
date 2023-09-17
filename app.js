d3a24de0d3f81c2ad48a0f6cf070af8f5edfc429
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-09-18 01:03:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -40,53 +40,53 @@
 
 // task 4
 
-var a = prompt("Write any number")
-var num_5 = 5
-if(a){
-    var b = a * 1
-document.write(a + " x " + "1 "  + "= "  + b+"<br>")
-var c = a * 2
-document.write(a + " x " + "2 "  + "= "  + c+"<br>")
-var d = a * 3
-document.write(a + " x " + "3 "  + "= "  + d+"<br>")
-var e = a * 4
-document.write(a + " x " + "4 "  + "= "  + e+"<br>")
-var f = a * 5
-document.write(a + " x " + "5 "  + "= "  + f+"<br>")
-var g = a * 6
-document.write(a + " x " + "6 "  + "= "  + g+"<br>")
-var h = a * 7
-document.write(a + " x " + "7 "  + "= "  + h+"<br>")
-var i = a * 8
-document.write(a + " x " + "8 "  + "= "  + i+"<br>")
-var j = a * 9
-document.write(a + " x " + "9 "  + "= "  + j+"<br>")
-var k = a * 10
-document.write(a + " x " + "10 "  + "= "  + k+"<br>")
-}
+// var a = prompt("Write any number")
+// var num_5 = 5
+// if(a){
+//     var b = a * 1
+// document.write(a + " x " + "1 "  + "= "  + b+"<br>")
+// var c = a * 2
+// document.write(a + " x " + "2 "  + "= "  + c+"<br>")
+// var d = a * 3
+// document.write(a + " x " + "3 "  + "= "  + d+"<br>")
+// var e = a * 4
+// document.write(a + " x " + "4 "  + "= "  + e+"<br>")
+// var f = a * 5
+// document.write(a + " x " + "5 "  + "= "  + f+"<br>")
+// var g = a * 6
+// document.write(a + " x " + "6 "  + "= "  + g+"<br>")
+// var h = a * 7
+// document.write(a + " x " + "7 "  + "= "  + h+"<br>")
+// var i = a * 8
+// document.write(a + " x " + "8 "  + "= "  + i+"<br>")
+// var j = a * 9
+// document.write(a + " x " + "9 "  + "= "  + j+"<br>")
+// var k = a * 10
+// document.write(a + " x " + "10 "  + "= "  + k+"<br>")
+// }
 
-else{
-    var b = num_5 * 1
-    document.write(num_5 + " x " + "1 "  + "= "  + b+"<br>")
-    var c = num_5 * 2
-    document.write(num_5 + " x " + "2 "  + "= "  + c+"<br>")
-    var d = num_5 * 3
-    document.write(num_5 + " x " + "3 "  + "= "  + d+"<br>")
-    var e = num_5 * 4
-    document.write(num_5 + " x " + "4 "  + "= "  + e+"<br>")
-    var f = num_5 * 5
-    document.write(num_5 + " x " + "5 "  + "= "  + f+"<br>")
-    var g = num_5 * 6
-    document.write(num_5 + " x " + "6 "  + "= "  + g+"<br>")
-    var h =num_5 * 7
-    document.write(num_5 + " x " + "7 "  + "= "  + h+"<br>")
-    var i = num_5 * 8
-    document.write(num_5 + " x " + "8 "  + "= "  + i+"<br>")
-    var j = num_5 * 9
-    document.write(num_5 + " x " + "9 "  + "= "  + j+"<br>")
-    var k = num_5 * 10
-    document.write(num_5 + " x " + "10 "  + "= "  + k+"<br>")
-}
+// else{
+//     var b = num_5 * 1
+//     document.write(num_5 + " x " + "1 "  + "= "  + b+"<br>")
+//     var c = num_5 * 2
+//     document.write(num_5 + " x " + "2 "  + "= "  + c+"<br>")
+//     var d = num_5 * 3
+//     document.write(num_5 + " x " + "3 "  + "= "  + d+"<br>")
+//     var e = num_5 * 4
+//     document.write(num_5 + " x " + "4 "  + "= "  + e+"<br>")
+//     var f = num_5 * 5
+//     document.write(num_5 + " x " + "5 "  + "= "  + f+"<br>")
+//     var g = num_5 * 6
+//     document.write(num_5 + " x " + "6 "  + "= "  + g+"<br>")
+//     var h =num_5 * 7
+//     document.write(num_5 + " x " + "7 "  + "= "  + h+"<br>")
+//     var i = num_5 * 8
+//     document.write(num_5 + " x " + "8 "  + "= "  + i+"<br>")
+//     var j = num_5 * 9
+//     document.write(num_5 + " x " + "9 "  + "= "  + j+"<br>")
+//     var k = num_5 * 10
+//     document.write(num_5 + " x " + "10 "  + "= "  + k+"<br>")
+// }
 
 
 
